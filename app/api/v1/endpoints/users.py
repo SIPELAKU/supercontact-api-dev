3edd6332f3daf8e63b9d97a0d0523df9d0ed725f
Afifudin <afifudinnu_ojt@solvera.id>
2025-12-19 10:31:34 +0700

    fix: sync with branch dev

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -3,7 +3,6 @@ from typing import Optional
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core import auth_require
 from app.db import get_async_session
 from app.schemas import ResponseModel, UserGetQuery
 from app.schemas.user_schema import PaginatedUserResponse
@@ -18,7 +17,7 @@ def get_user_service(db: AsyncSession = Depends(get_async_session)):
 
 @router.get(
     "",
-    dependencies=[Depends(auth_require)],
+    # dependencies=[Depends(auth_require)],
     response_model=ResponseModel[PaginatedUserResponse],
 )
 async def get_all_users(
